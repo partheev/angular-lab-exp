d819ac13ebf5c4f07bc618fc40104534d227a0d4
partheev <partheev8@gmail.com>
2023-08-18 08:50:23 +0530

    add exp. 2 (components) & exp. 3 (templates)

---
+++ README.md
@@ -0,0 +1,25 @@
+### Install angular CLI
+
+```
+npm install -g @angular/cli@^8.0.0
+```
+
+### Create angular project
+
+To create an application in Angular, use the below syntax −
+
+```
+ng new <project-name>
+```
+
+For example, this command ` ng new FoodDeliveryApp` creates an angular application with _FoodDeliveryApp_ name.
+
+### Create a component
+
+Components are building block of Angular. To create a component in angular use the below syntax −
+
+```
+ng g c <component-name>
+```
+
+For example, this command ` ng g c Header` creates Header component.
---
+++ exp2_components/MyApp/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
